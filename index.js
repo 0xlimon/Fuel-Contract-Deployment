ee0c46b1dc225c1223e5b39a4e2f35d75780adc7
0xLimon <44081423+0xlimon@users.noreply.github.com>
2024-07-01 15:21:45 +0330

    Update index.js

--- index.js
+++ index.js
@@ -90,13 +90,147 @@ const deployContract = async (contractPath, contractName, wallet) => {
     }
 };
 
+const sendTransaction = async () => {
+    const toAddress = readlineSync.question('Enter the recipient address: ');
+    const [minAmount, maxAmount] = readlineSync.question('Enter the token amount range (min,max): ').split(',').map(Number);
+    const [minDelay, maxDelay] = readlineSync.question('Enter the delay range between transactions in seconds (min,max): ').split(',').map(Number);
+
+    while (true) {
+        const amount = (Math.random() * (maxAmount - minAmount) + minAmount).toFixed(4);
+        const delay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+
+        const tx = {
+            to: toAddress,
+            value: amount,
+            gasLimit: 21000,
+        };
+
+        const response = await wallet.sendTransaction(tx);
+        console.log(chalk.blue(`Transaction sent with hash: ${response.transactionHash} - Amount: ${amount} FUEL - Delay: ${delay}s`));
+
+        await new Promise(resolve => setTimeout(resolve, delay * 1000));
+    }
+};
+
+const randomDelay = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const deployContractsRandomly = async (contractPath, contractName) => {
+    const deployedContracts = loadDeployedContractInfo();
+
+    for (const wallet of wallets) {
+        if (!deployedContracts[wallet.address]) {
+            const delay = randomDelay(5000, 10000); 
+            console.log(chalk.blue(`Waiting for ${delay / 1000} seconds before deploying contract for wallet ${wallet.address.slice(0, 6)}...`));
+            await new Promise(resolve => setTimeout(resolve, delay));
+            const contractAddress = await deployContract(contractPath, contractName, wallet);
+            saveDeployedContractInfo(wallet, contractAddress);
+        } else {
+            console.log(chalk.yellow(`Contract already deployed for wallet ${wallet.address.slice(0, 6)}...`));
+        }
+    }
+};
+
+const generateBiasedRandomNumber = () => {
+    const random = Math.random();
+    if (random < 0.33) {
+        return 3 * (Math.floor(Math.random() * 33) + 1); 
+    } else if (random < 0.66) {
+        return 5 * (Math.floor(Math.random() * 20) + 1);
+    } else {
+        return 15 * (Math.floor(Math.random() * 7) + 1); 
+    }
+};
+
+// تابع برای تعامل با کانترکت
+const interactWithContract = async () => {
+    const deployedContracts = loadDeployedContractInfo();
+    const walletAddresses = Object.keys(deployedContracts);
+
+    if (walletAddresses.length === 0) {
+        console.log(chalk.red('No contracts found. Please deploy a contract first.'));
+        return;
+    }
+
+    const [minTransactions, maxTransactions] = readlineSync.question('Enter the daily transaction range (min,max): ').split(',').map(Number);
+
+    const walletTransactions = walletAddresses.map(address => {
+        return {
+            address,
+            dailyTransactions: randomDelay(minTransactions, maxTransactions)
+        };
+    });
+
+    console.log(chalk.yellow('Daily transactions assigned for each wallet:'));
+    walletTransactions.forEach(({ address, dailyTransactions }) => {
+        console.log(chalk.yellow(`Wallet: ${address.slice(0, 6)}... | Daily Transactions: ${dailyTransactions}`));
+    });
+
+    const interact = async (walletAddress, dailyTransactions) => {
+        const walletInfo = deployedContracts[walletAddress];
+        const walletInstance = Wallet.fromPrivateKey(walletInfo.privateKey, provider);
+        const contractAddress = walletInfo.contractAddress;
+        const abiPath = path.join(__dirname, 'FizzBuzzContract', 'out/debug', `FizzBuzz-abi.json`);
+        const abi = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
+        const contract = new Contract(contractAddress, abi, walletInstance);
+
+        const totalMillisecondsInDay = 24 * 60 * 60 * 1000;
+        const delays = Array.from({ length: dailyTransactions - 1 }, () => randomDelay(1, totalMillisecondsInDay));
+        delays.sort((a, b) => a - b);
+
+        for (let i = 0; i < dailyTransactions; i++) {
+            try {
+                const input = generateBiasedRandomNumber();
+                const response = await contract.functions.fizzbuzz(input).txParams({ gasLimit: 100000 }).call();
+                const result = response.value;
+
+                // نمایش نتیجه FizzBuzz
+                let resultText = '';
+                switch (result) {
+                    case 'Fizz':
+                        resultText = 'Fizz';
+                        break;
+                    case 'Buzz':
+                        resultText = 'Buzz';
+                        break;
+                    case 'FizzBuzz':
+                        resultText = 'FizzBuzz';
+                        break;
+                    default:
+                        if (result && typeof result === 'object' && 'Other' in result) {
+                            resultText = `Other: ${result.Other}`;
+                        } else {
+                            resultText = 'Unknown result';
+                        }
+                        break;
+                }
+
+                if (i < dailyTransactions - 1) {
+                    const delay = delays[i] - (delays[i - 1] || 0); 
+                    console.log(chalk.green(`Wallet: ${walletAddress.slice(0, 6)}... | Contract: ${contractAddress.slice(0, 6)}... | Transaction Hash: ${response.transactionId} | Result: ${resultText} | Next interaction: ${delay / 1000} seconds`));
+                    await new Promise(resolve => setTimeout(resolve, delay));
+                } else {
+                    console.log(chalk.green(`Wallet: ${walletAddress.slice(0, 6)}... | Contract: ${contractAddress.slice(0, 6)}... | Transaction Hash: ${response.transactionId} | Result: ${resultText} | All interactions completed for today`));
+                }
+            } catch (error) {
+                console.error(chalk.red(`Failed to interact with contract: ${error.message}`));
+                console.error(chalk.red(`Error details: ${JSON.stringify(error.metadata, null, 2)}`));
+            }
+        }
+    };
+
+    const interactionPromises = walletTransactions.map(({ address, dailyTransactions }) => interact(address, dailyTransactions));
+    await Promise.all(interactionPromises);
+};
+
 const main = async () => {
     printHeader();
     console.log(chalk.green(`Number of wallets: ${wallets.length}`));
     while (true) {
         console.log(chalk.cyan('\nMenu:'));
         console.log(chalk.cyan('1. Deploy Contract'));
-        console.log(chalk.cyan('2. Exit'));
+        console.log(chalk.cyan('2. Send Transaction'));
+        console.log(chalk.cyan('3. Interact with Contract'));
+        console.log(chalk.cyan('4. Exit'));
 
         const choice = readlineSync.questionInt('Enter your choice: ');
 
@@ -107,6 +241,12 @@ const main = async () => {
                 await deployContractsRandomly(contractPath, contractName);
                 break;
             case 2:
+                await sendTransaction();
+                break;
+            case 3:
+                await interactWithContract();
+                break;
+            case 4:
                 console.log(chalk.cyan('Exiting...'));
                 return;
             default:
